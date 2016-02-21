2468fd823759ba9def7d0dea4a6a5fd7828f4c97
michaelbasnak <root@example.com>
2016-02-21 16:48:51 -0700

    add group class, group activity and temp buttons

--- app/src/main/java/schedulink/schedulink/GroupActivity.java
+++ app/src/main/java/schedulink/schedulink/GroupActivity.java
@@ -1,6 +1,5 @@
 package schedulink.schedulink;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class GroupsActivity extends AppCompatActivity {
+public class GroupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_groups);
+        setContentView(R.layout.activity_group);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -25,10 +24,6 @@ public class GroupsActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
-
-    public void displayGroupActivity(View v){
-        startActivity(new Intent(GroupsActivity.this, GroupActivity.class));
     }
 
 }
